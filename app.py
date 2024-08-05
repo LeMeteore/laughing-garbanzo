76312467df16fa6a670969f1e88fcf56e6de06f3
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2024-08-05 20:04:28 +0000

    Update app module

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+# mes importations
 import streamlit as st
+import pandas as pd
 
-st.write("Bonjour DIT, comment allez vous ?")
+# un message de bienvenue
+st.write("Bonjour DIT, comment allez vous ? Ici tout va bien")
+
+
+# un dataframe
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+# affichage du dataframe
+st.write(df)
