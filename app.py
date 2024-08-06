6ab09acf28376e436f4c96b7ebf67baf41270d40
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2024-08-06 19:28:12 +0000

    Add a title

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 # un message de bienvenue
 st.write("Bonjour DIT, comment allez vous ? Ici tout va bien")
 
+st.markdown("### Ceci est un titre énorme")
 
 # un dataframe
 df = pd.DataFrame({
